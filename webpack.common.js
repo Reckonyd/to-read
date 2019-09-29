c9972ad5011727bb1db97ea51f03b50bfd9f6434
Thanos Valimitis <thanos.valimitis@gmail.com>
2019-09-29 16:48:37 +0300

    Seperate Config For Production and Development

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Todo Learn',
+      template: 'src/index.html',
+    }),
+  ],
+}
---
+++ webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+})
