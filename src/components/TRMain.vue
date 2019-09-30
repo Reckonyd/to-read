4a4b84898630de718fc95f280df29fd0f8cbe4b0
Thanos Valimitis <thanos.valimitis@gmail.com>
2019-09-30 11:37:01 +0300

    Added debounce to ease the load on seaching array

--- src/components/TRMain.vue
+++ src/components/TRMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TRSearch @searching="onSearch"></TRSearch>  
-    <TRListView :toReadList="searchList.length === 0 ? list : searchList"></TRListView>  
+    <TRSearch @searching="onSearch"></TRSearch>
+    <TRListView :toReadList="searchList.length === 0 ? list : searchList"></TRListView>
   </div>
 </template>
 
@@ -10,38 +10,35 @@ import TRSearch from './TRSearch.vue'
 import TRListView from './TRListView.vue'
 import axios from 'axios'
 
-
 export default {
-  name: "trmain",
+  name: 'trmain',
   data() {
     return {
       list: [],
-      searchList: []
+      searchList: [],
     }
   },
   methods: {
-    onSearch: function(search='') {
-      this.searchList = this.list.filter(item =>{ 
-         return item.author.toLowerCase().indexOf(search.toLowerCase()) === 0
+    onSearch: function(search = '') {
+      this.searchList = this.list.filter(item => {
+        return item.author.toLowerCase().indexOf(search.toLowerCase()) === 0
       })
-    }
+    },
   },
   mounted() {
-    axios
-      .get('https://picsum.photos/v2/list')
-      .then(responce => {
-        const list = responce.data
-        list.forEach(({id, author, download_url}) => this.list.push({id, author, download_url}))
-      })
-    
+    axios.get('https://picsum.photos/v2/list').then(responce => {
+      const list = responce.data
+      list.forEach(({ id, author, download_url }) =>
+        this.list.push({ id, author, download_url }),
+      )
+    })
   },
   components: {
     TRSearch,
-    TRListView
-  }
+    TRListView,
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- src/components/TRSearch.vue
+++ src/components/TRSearch.vue
@@ -1,25 +1,39 @@
 <template>
   <div>
-    <p>{{search}}</p>
-    <form @submit.prevent="">
+    <form @submit.prevent>
       <label for="search">Search</label>
-      <input @input="$emit('searching', search)" type="text" v-model="search" id="search">
+      <input @input="debouncedSearch()" type="text" v-model="search" id="search" />
     </form>
+    <span>{{searching ? 'Waiting for your to stop' : ''}}</span>
   </div>
 </template>
 
 <script>
+import debounce from 'lodash/debounce'
+
 export default {
   name: 'trsearch',
   data() {
     return {
-      search: ''
+      search: '',
+      searching: false,
     }
+  },
+  watch: {
+    search: function() {
+      this.searching = true
+      this.debouncedSearch()
+    },
+  },
+  methods: {
+    debouncedSearch: debounce(function() {
+      this.searching = false
+      this.$emit('searching', this.search)
+    }, 500),
   },
   props: [],
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
