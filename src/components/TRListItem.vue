c66f4c8f73d998ed741ff6a3a22c9caeaea729b0
Thanos Valimitis <thanos.valimitis@gmail.com>
2019-10-08 12:04:53 +0300

    Changed ListView - ListItem props and image preview

--- src/components/TRListItem.vue
+++ src/components/TRListItem.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <TRDelete class="relative" @delete="deleteItem(id)" />
-    <a :href="url" target="_blank" rel="noopener noreferrer">
+    <a :href="toReadItem.url" target="_blank" rel="noopener noreferrer">
       <img
-        class="object-cover object-center mx-auto w-full h-64 rounded-t shadow-md"
-        :src="image"
-        :alt="imageAlt"
+        :class="[
+          !toReadItem.encoded ? imageStyle : '',
+          'mx-auto',
+          'w-full',
+          'rounded-t',
+          'shadow-md',
+        ]"
+        :src="toReadItem.image_url"
+        :alt="toReadItem.image_alt || `Image of ${toReadItem.title}`"
       />
     </a>
-    <h2 class="my-4 mx-3 font-bold tracking-wider text-2xl">{{ title }}</h2>
-    <p class="mb-4 mx-5 text-lg text-left leading-tight">
-      {{ description }}
+    <h2 class="my-4 mx-3 font-bold tracking-wider text-xl">
+      {{ toReadItem.title }}
+    </h2>
+    <p class="mb-5 mx-6 text-lg text-left leading-tight">
+      {{ toReadItem.description }}
     </p>
   </div>
 </template>
@@ -25,31 +33,15 @@ export default {
     TRDelete,
   },
   props: {
-    id: {
-      type: Number,
+    toReadItem: {
+      type: Object,
       required: true,
     },
-    url: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    imageAlt: {
-      type: String,
-      default: `Image of`,
-      required: false,
-    },
+  },
+  data: function() {
+    return {
+      imageStyle: ['object-contain', 'h-64'],
+    }
   },
   methods: {
     ...mapActions(['deleteItem']),
--- src/components/TRListView.vue
+++ src/components/TRListView.vue
@@ -2,14 +2,9 @@
   <div>
     <TRListItem
       v-for="item in list"
-      :id="item.id"
       :key="item.id"
-      :url="item.url"
-      :title="item.title"
-      :description="item.description"
-      :image="item.image_url"
-      :image-alt="item.image_alt"
-      class="flex flex-col lg:w-5/12 bg-gray-700 text-gray-100 my-2 mx-1 sm:mx-2 text-center border-b-8 border-red-700 rounded-t shadow overflow-hidden"
+      :to-read-item="item"
+      class="flex flex-col lg:w-5/12 bg-gray-700 text-gray-100 my-2 mx-1 sm:mx-2 text-center border-b-8 border-red-700 rounded-t shadow"
     ></TRListItem>
   </div>
 </template>
