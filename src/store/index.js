1c91ebfe02b7871059da81cb0192eebd0821c581
Thanos Valimitis <thanos.valimitis@gmail.com>
2019-10-09 16:03:13 +0300

    Fixed item loading from Local Storage

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,8 @@ export default new Vuex.Store({
         }
       }
 
+      list.sort((a, b) => a.id - b.id)
+
       commit('INIT_LIST', list)
     },
     async addItem({ commit, dispatch, state }, url) {
