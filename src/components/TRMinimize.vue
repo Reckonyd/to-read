7a94d50551f8e0d016b9743f1d924e09aabd391b
Thanos Valimitis <thanos.valimitis@gmail.com>
2019-10-13 11:23:06 +0300

    Added Minimize Button

---
+++ src/components/TRMinimize.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button
+      class="btn absolute top-0 right-0 mr-10 px-3 py-1 shadow opacity-75"
+      @click="$emit('minimize')"
+    >
+      &#x0005F;
+    </button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
